abcf6001d260fe481da6a7745fb2cacabc5fdbe8
awjames6875 <ajames@growthgenix.ai>
2025-11-16 21:10:58 -0600

    Add layout component prop types

--- components/layout/index.ts
+++ components/layout/index.ts
@@ -2,6 +2,6 @@
 export { default as Navigation } from './Navigation';
 export { default as Footer } from './Footer';
 
-// Re-export types if needed
-export type { default as NavigationProps } from './Navigation';
-export type { default as FooterProps } from './Footer';
+// Re-export types
+export type { NavigationProps } from './Navigation';
+export type { FooterProps } from './Footer';
